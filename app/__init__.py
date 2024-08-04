b9831f4347bf519f6a51087f4f16410ea1134b22
Raikojou <48170127+Raikojou@users.noreply.github.com>
2024-08-04 12:07:42 +1000

    Added CORS

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
+from flask_cors import CORS
 
 def create_app():
     app = Flask(__name__)
-
+    CORS(app, resources={r"/*": {"origins": "https://tileselection.com.au"}})
+    
     from .blueprints.check_availability_blueprint import check_availability_bp
     #from .blueprints.place_order_blueprint import place_order_bp
     app.register_blueprint(check_availability_bp, url_prefix='/check_availability')
